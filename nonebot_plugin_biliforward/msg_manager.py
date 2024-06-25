77417a53b0a11152756a8b49f8e41692e14e3a07
ShawRong <shawnrong1213@gmail.com>
2024-06-25 17:56:43 +0800

    add new file msg_manager.py

---
+++ nonebot_plugin_biliforward/msg_manager.py
@@ -0,0 +1,21 @@
+#todo
+class Msg_manager:
+    @staticmethod
+    def load_from_database():
+        pass
+    
+    @staticmethod
+    def save2database():
+        pass
+    
+    def __init__(self, msg_queue):
+        self.msg_queue = msg_queue
+
+    def empty():
+        pass
+    
+    def put():
+        pass
+
+    def get():
+        pass
